fb079a0bf61c8a782d551fafe685ce1dd753a0d4
leonardoserra <serra.leonardo92@gmail.com>
2023-09-25 15:16:59 +0200

    test per Add fatto

---
+++ TestCalculator/UnitTest1.cs
@@ -0,0 +1,22 @@
+using net_calculator_tester;
+
+namespace TestCalculator
+{
+    public class CalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test(Description="AddTestDifferentValues")]
+        [TestCase(-2,34)]
+        [TestCase(0,-34)]
+        [TestCase(0.44f,-34.352f)]
+        public void Add_Test(float num1,float num2)
+        {
+            Calculator.Add(num1, num2);
+            Assert.Pass();
+        }
+    }
+}
