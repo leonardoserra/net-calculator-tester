bf224e6b8012a8eccdc6c238adbd214a3bc49c0e
leonardoserra <serra.leonardo92@gmail.com>
2023-09-25 15:19:13 +0200

    ok subtract

--- TestCalculator/UnitTest1.cs
+++ TestCalculator/UnitTest1.cs
@@ -18,5 +18,15 @@ namespace TestCalculator
             Calculator.Add(num1, num2);
             Assert.Pass();
         }
+
+        [Test(Description = "SubtractTestDifferentValues")]
+        [TestCase(-2, 34)]
+        [TestCase(0, -34)]
+        [TestCase(0.44f, -34.352f)]
+        public void Subtract_Test(float num1, float num2)
+        {
+            Calculator.Subtract(num1, num2);
+            Assert.Pass();
+        }
     }
 }
