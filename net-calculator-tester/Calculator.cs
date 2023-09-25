de45c0099756d95de56ca4c0b433b582bb2713a3
leonardoserra <serra.leonardo92@gmail.com>
2023-09-25 14:58:14 +0200

    ok class Calculator

---
+++ net-calculator-tester/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester
+{
+    public static class Calculator
+    {
+        public static float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+        public static float Subtract(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+        public static float Divide(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+        public static float Multiply(float num1, float num2)
+        {
+            return num1 *num2;
+        }
+    }
+}
